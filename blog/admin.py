139959f7e5632f3822c2b3d0a51a029dc3f0d11c
Arif <arif.asan.jmi@gmail.com>
2021-05-26 22:03:26 +0530

    Module added for contactUs

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ContactUs
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subject', 'email', 'created_at')
+    list_filter = ('created_at',)
+
+
+admin.site.register(ContactUs, ContactAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class ContactUs(models.Model):
+
+    name = models.CharField(max_length=200, null=False)
+    phone = models.CharField(max_length=200, null=False)
+    subject = models.CharField(max_length=200, null=False, default="")
+    email = models.EmailField(max_length=200, null=False)
+    content = models.TextField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Contact Us"
+        ordering = ('-created_at',)
+    def __str__(self):
+        return "Name"
+
+
+
+
+
