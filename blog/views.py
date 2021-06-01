fb34e48139670579f6ec8ee7560a2cfc62a18fb7
Arif <arif.asan.jmi@gmail.com>
2021-06-01 13:41:54 +0530

    Contact us Integrated with frontend

--- blog/views.py
+++ blog/views.py
@@ -6,18 +6,18 @@ from .models import ContactUs
 # Create your views here.
 def contactUs(request):
     if request.method == "POST":
-        name = request.POST("name")
-        email = request.POST("email")
-        phone = request.POST("phone")
-        subject = request.POST("subject")
-        content = request.POST("content")
+        name = request.POST["name"]
+        email = request.POST["email"]
+        phone = request.POST["phone"]
+        subject = request.POST["subject"]
+        content = request.POST["content"]
 
-        if len(name)<3 or len(email)<5 or len(phone)<10 or len(subject)<8 or len(content)<15:
-            messages.error(request, "Please fill the form correctly")
+        # if len(name)<3 or len(email)<5 or len(phone)<10 or len(subject)<8 or len(content)<15:
+        #     messages.error(request, "Please fill the form correctly")
 
-        else:
-            contact = ContactUs(name=name, email=email, phone=phone,subject=subject,content=content)
-            contact.save()
-            messages.success(request, "We got your message")
+        # else:
+        contact = ContactUs(name=name, email=email, phone=phone,subject=subject,content=content)
+        contact.save()
+        # messages.success(request, "We got your message")
 
     return render(request, "contact.html")
