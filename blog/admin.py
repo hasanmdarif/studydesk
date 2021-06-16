426369028e0c31fdb2faac8aa762321a89d50701
Nehajaiswal868 <nehajaiswal412001@gmail.com>
2021-06-15 23:53:02 -0700

    changes

--- blog/admin.py
+++ blog/admin.py
@@ -7,4 +7,15 @@ class ContactAdmin(admin.ModelAdmin):
     list_filter = ('created_at',)
 
 
-admin.site.register(ContactUs, ContactAdmin)
+from django.contrib import admin
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
--- blog/urls.py
+++ blog/urls.py
@@ -4,3 +4,11 @@ from django.urls import path, include
 urlpatterns = [
     path('contact/', views.contactUs, name='contact')
 ]
+
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+]
