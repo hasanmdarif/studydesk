6974a760830910d22eb6f6ba2fcf12ddc331a47a
Nehajaiswal868 <nehajaiswal412001@gmail.com>
2021-06-16 22:49:27 -0700

    changes

--- blog/urls.py
+++ blog/urls.py
@@ -4,3 +4,9 @@ from django.urls import path, include
 urlpatterns = [
     path('contact/', views.contactUs, name='contact')
 ]
+
+from django.contrib import admin
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
--- blog/views.py
+++ blog/views.py
@@ -21,3 +21,15 @@ def contactUs(request):
         # messages.success(request, "We got your message")
 
     return render(request, "contact.html")
+
+
+from django.views import generic
+from .models import Post
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
